9d9ff8c576af431f101199b7ee8d7531a3cf0e32
EC2 Default User <ec2-user@ip-172-31-5-103.ap-southeast-1.compute.internal>
2019-03-23 10:11:30 +0000

    ch11前半,next => ユーザーのcreateアクションを更新

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,8 +31,11 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  host = '753d35223faa4837af27c6f5a1a3f3ae.vfs.cloud9.ap-southeast-1.amazonaws.com' 
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+  
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
